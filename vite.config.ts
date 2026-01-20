1ff5b5a9f80821ace2d4c303953744a0398bcb66
André Elias Perry da Câmara <eliaspc@yahoo.com>
2026-01-20 13:15:28 +0000

    Refactor path aliases in vite.config.ts to use absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,10 +7,10 @@ export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            '@frontend': path.resolve(__dirname, './app/frontend'),
-            '@orchestrator': path.resolve(__dirname, './app/orchestrator'),
-            '@backend': path.resolve(__dirname, './app/backend'),
-            '@shared': path.resolve(__dirname, './app/shared'),
+            '@frontend': '/app/frontend',
+            '@orchestrator': '/app/orchestrator',
+            '@backend': '/app/backend',
+            '@shared': '/app/shared',
         },
     },
 })
